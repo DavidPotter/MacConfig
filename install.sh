37083193e2483a4352cba79b1a2f4b955df972d8
David Potter <DavidP@inrix.com>
2011-05-01 20:41:49 -0700

    Fix message again.

--- install.sh
+++ install.sh
@@ -68,8 +68,16 @@ done
 # and set up the correct hosts file.
 source $REPOLOC/installChangeHosts.sh
 
+if [ -n $BASE_SOURCE ]
+then
+	SCRIPTNAME=$BASE_SOURCE
+else
+	SCRIPTNAME=$0
+fi
+SCRIPTNAME="`basename $SCRIPTNAME`"
+
 echo '#'
-echo "# `basename $BASH_SOURCE`:"
+echo "# $SCRIPTNAME:"
 echo '# Invoke the following command to complete the installation:'
 echo '#   source ~/.bash_profile'
 echo '#"
