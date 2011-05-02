060b04fc5e04f24f83bb883760b8c97ba7c9c52c
David Potter <DavidP@inrix.com>
2011-05-01 20:46:26 -0700

    Fix the message one more time again by removing the name of the script since $BASH_SOURCE and $0 are both empty.

--- install.sh
+++ install.sh
@@ -68,16 +68,7 @@ done
 # and set up the correct hosts file.
 source $REPOLOC/installChangeHosts.sh
 
-if [ -n $BASH_SOURCE ]
-then
-	SCRIPTNAME=$BASH_SOURCE
-else
-	SCRIPTNAME=$0
-fi
-SCRIPTNAME="`basename $SCRIPTNAME`"
-
 echo '#'
-echo "# $SCRIPTNAME:"
 echo '# Invoke the following command to complete the installation:'
 echo '#   source ~/.bash_profile'
-echo '#"
+echo '#'
