6e15d6ff32cfe64cd0c9eb3b6ee64e050f7c9be8
David Potter <dpotter@tableau.com>
2018-07-26 17:57:58 -0700

    Make the definition of LSCOLORS more understandable

--- bash.d/2-env.sh
+++ bash.d/2-env.sh
@@ -1,19 +1,38 @@
 #!/bin/bash -e
 
 # Set the colors of a directory listing.
-# These colors look good on the Grass terminal theme.
-#   Directory            = g:cyan
-#   Symbolic link        = C:bold green
-#   Socket               = F:bold magenta
-#   Pipe                 = f:magenta
-#   Executable           = h:light grey
-#   Block special        = e:blue g:cyan background
-#   Character special    = e:blue d:brown background
-#   Executable/setuid    = H:bold light grey
-#   Executable/setgid    = H:bold light grey
-#   directory w/sticky   = G:bold cyan
-#   directory w/o sticky = G:bold cyan
-export LSCOLORS=gxCxFxfxhxegedHxHxGxGx
+# Colors:
+#   a black     d brown     g cyan
+#   b red       e blue      h light grey
+#   c green     f magenta   x default color
+# Positions:
+#   Directory
+#   Symbolic link - special kind of file that contains reference to another file or directory
+#   Socket        - special kind of file used for inter-process communication
+#   Pipe          - special file that connects the output of one process to the input of another
+#   Executable
+#   Block special     - A kind of device file
+#   Character special - A kind of device file
+#   Executable with setuid bit set
+#   Executable with setgid bit set
+#   Directory writable to others with sticky bit - only owner can rename or delete files
+#   Directory writable to others without sticky bit - any user with write and execution permissions can rename or delete files
+function __setLsColors() {
+    c01='gx' # directory            - cyan
+    c02='Cx' # symbolic link        - bold green
+    c03='Fx' # socket               - bold magenta
+    c04='fx' # pipe                 - magenta
+    c05='hx' # executable           - light grey
+    c06='eg' # block special        - blue w/cyan background
+    c07='ed' # character special    - blue w/brown background
+    c08='Hx' # executable/setuid    - bold light grey
+    c09='Hx' # executable/setgid    - bold light grey
+    c10='Gx' # directory w/sticky   - bold cyan
+    c11='Gx' # directory w/o sticky - bold cyan
+    export LSCOLORS="${c01}${c02}${c03}${c04}${c05}${c06}${c07}${c08}${c09}${c10}${c11}"
+}
+__setLsColors
+unset __setLsColors
 
 # Set the default editor to TextMate.
 # export EDITOR=/usr/local/bin/mate
